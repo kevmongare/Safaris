66adf6aabeebf3646a03e069c26923e90264d400
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-19 16:04:11 +0300

    update: on the menu

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from '../src/pages/masaiMara'
+import App from './App'
 // import App from './pages/masaiMara'
 
 
