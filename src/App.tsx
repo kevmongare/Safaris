6ee46b448fbc3743da764dc98bfbfccfa0c8fbfd
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-19 09:50:56 +0300

    update: update of the components of the website

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Menu from './components/Menu';
 import Blog from '../src/components/blog';
 import Whatsapp from './components/whatsapp'
 import AboutUs from './components/aboutUs';
-import Details from './components/details';
+// import Details from './components/details';
 import PackageCard from './components/packagescard'
 import Hero from './components/hero';
 import Contact from './components/contact';
