6c08d8a9cff7fb6dac77f5ab7df615d4b606aada
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-24 18:51:23 +0300

    update ont the app structure

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import Menu from './components/Menu';
 import Blog from '../src/components/blog';
 import Whatsapp from './components/whatsapp'
 import AboutUs from './components/aboutUs';
-import Cards from './components/titleCards';
-// import Details from './components/details';
+// import Cards from './components/titleCards';
+// import Destinations from './components/Destinations';
+import Details from './components/details';
+// import DestinationHighlights from './components/DestinationHighlights';
 // import PackageCard from './components/packagescard'
 import Hero from './components/hero';
 // import Contact from './components/contact';
@@ -24,15 +26,23 @@ function App() {
     <>
       <Menu />
       <Hero />
-       <AboutUs />
-       <Cards />
+       <Details />
+       {/* <Destinations />
+       <DestinationHighlights />
+       
+        */}
+       
+       {/* <Cards /> */}
        {/* <PackageCard /> */}
+       
+       
        <Whatsapp />
         <Blog />
+        <AboutUs />
         {/* <Contact /> */}
         <Footer />
         
-        {/* <Details /> */}
+        
     </>
   )
 }
