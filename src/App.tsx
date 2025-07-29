eef0225624ea047790e70c984553eb21c5ba6c51
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-29 20:10:52 +0300

    major update on all components

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,56 @@
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Menu from './components/Menu';
 import Blog from '../src/components/blog';
-import Whatsapp from './components/whatsapp'
+import Whatsapp from './components/whatsapp';
 import AboutUs from './components/aboutUs';
 import TopDestinations from './components/TopDestinations';
 import About from './components/About';
 import DestinationHighlights from './components/DestinationHighlights';
 import Hero from './components/hero';
 import Contact from './components/contact';
-import Footer from './components/footer'
-import './App.css'
-// import { FaEnvelope, FaPhone } from 'react-icons/fa';
+import Footer from './components/footer';
+import Destination from '../src/pages/destinationdemo'
+import './App.css';
+import Kenya from '../src/pages/Kenya';
+import 'swiper/swiper-bundle.css';
 
-
-
-import 'swiper/swiper-bundle.css'; 
-
-
-
-function App() {
-  
-
+// Layout component for Menu and Footer
+const MainLayout = () => {
   return (
     <>
       <Menu />
-      <Hero />
-       <About />
-       <TopDestinations />
-       <DestinationHighlights />
-       <Whatsapp />
-       <AboutUs />
-        <Blog />     
-        <Contact />
-        <Footer />
-        
-        
+      <Outlet />  {/* This renders the child route components */}
+      <Footer />
     </>
-  )
+  );
+};
+
+function App() {
+  return (
+    <Routes>
+      {/* Routes using MainLayout */}
+      <Route element={<MainLayout />}>
+        {/* Landing page with all components */}
+        <Route path="/" element={
+          <>
+            <Hero />
+            <About />
+            <TopDestinations />
+            <DestinationHighlights />
+            <Whatsapp />
+            <AboutUs />
+            <Blog />
+            <Contact />
+          </>
+        } />
+        
+        {/* Other pages (only Menu + Page Content + Footer) */}
+        <Route path="/Kenya" element={<Kenya />} />
+        <Route path='/Destination' element = {<Destination/>}/>
+        {/* Add more routes here as needed */}
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom' // Import BrowserRouter
 // import App from './pages/masaiMara'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+     <BrowserRouter>
     <div className='bg-[var(--primary)]'>
       <App />
     </div>
+    </BrowserRouter>
   </StrictMode>,
 )
