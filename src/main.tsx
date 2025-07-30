6e7c5a58c5580d374679d01dddf48fd797c194bc
Kevin Mongare <117772705+kevmongare@users.noreply.github.com>
2025-07-30 21:45:43 +0300

    update on all files

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // Import BrowserRouter
 import './index.css'
 import App from './App'
-import { BrowserRouter } from 'react-router-dom' // Import BrowserRouter
-// import App from './pages/masaiMara'
 
 
 createRoot(document.getElementById('root')!).render(
